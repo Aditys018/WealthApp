e554a33b5fadaeeb770f8a7c8dc5a45bea92f0de
Divyansh Anand <div.gyan09@gmail.com>
2025-05-25 20:43:52 +0530

    feat: modify the sales trend api to fetch query from api

--- backend/src/controller/salestrend.controller.js
+++ backend/src/controller/salestrend.controller.js
@@ -4,6 +4,13 @@ const ATTOM_API_KEY = "283129a21a7f8361fee7404e88463f15";
 
 const getSalesTrend = async (req, res) => {
   try {
+    const {startYear, endYear, geoId, interval='yearly'} = req.query;
+    console.log("Fetching sales trend for:", {
+      startYear,
+      endYear,
+      geoId,
+      interval
+    });
     const response = await axios.get(
       "https://api.gateway.attomdata.com/v4/transaction/salestrend",
       {
@@ -12,10 +19,10 @@ const getSalesTrend = async (req, res) => {
           apikey: ATTOM_API_KEY,
         },
         params: {
-          geoIdV4: "6828b00047035292dd47fe020e636bb3",
-          interval: "yearly",
-          startyear: 2018,
-          endyear: 2022,
+          geoIdV4: geoId,
+          interval,
+          startyear: startYear,
+          endyear: endYear,
         },
       }
     );
